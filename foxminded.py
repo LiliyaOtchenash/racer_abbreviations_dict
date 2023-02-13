5f14882bc7b18026a0d2cbfa25d5ad5ef1c27562
lothcneash <liliya.otchenash28@gmail.com>
2023-02-13 10:32:07 +0200

    fix

--- foxminded.py
+++ foxminded.py
@@ -1,75 +1,83 @@
-
 import re
 import datetime
 from collections import OrderedDict
+from datetime import datetime
+from typing import Dict
 
-def create_racer_abbreviations_dict(file_name):
-    """Retrieves {'abbreviation': (name, team)}" format dict from abbreviations.txt"""
-    abbreviations_dict = {}
-    with open(file_name, 'r') as fn:
-        for line in fn:
-            matchObj = re.match(r'^(\w+)_([a-zA-Z\s]+)_([a-zA-Z\s]+)$', line)
-            # group(1) is abbreviation, i.e 'SVM'
-            abbreviations_dict[matchObj.group(1)] = (
-                matchObj.group(2),  # name of a pilot
-                matchObj.group(3).rstrip(),  # team
+
+def create_racer_abbreviations_dict(file_name) -> Dict[str, tuple[str, str]]:
+    """
+    Retrieves {'abbreviation': (name, team)}
+    format dict from abbreviations.txt
+    """
+    abbreviations = {}
+    with open(file_name, 'r') as file:
+        for line in file:
+            match_obj = re.match(r'^(\w+)_([a-zA-Z\s]+)_([a-zA-Z\s]+)$', line)
+            abbreviations[match_obj.group(1)] = (
+                match_obj.group(2),
+                match_obj.group(3).rstrip()
             )
-    return abbreviations_dict
+    return abbreviations
 
 
-# {'abbreviation of pilot': ('name of pilot, 'team')}
-abbr_dict = create_racer_abbreviations_dict(
-    'abbreviations.txt')
+def retrieve_timings_from_log(file_name) -> Dict[str, datetime]:
+    """
+     matches 2 groups: abbreviation of a racer and time
+     returns timing log from start.log or end.log in
+      {'abbreviation': time} format
+    """
 
-
-# returns timing log from start.log or end.log in {'abbreviation': time} format
-def retrieve_timings_from_log(file_name):
     timing_log = {}
-    with open(file_name, 'r') as fn:
-        for line in fn:
-            # matches 2 groups: abbreviation of a racer and time
-            matchObj = re.match(r'^([A-Z]+).*(\d{2}:\d+:\d+\.\d+)$', line)
+    with open(file_name, 'r') as file:
+        for line in file:
+            match_obj = re.match(r'^([A-Z]+).*(\d{2}:\d+:\d+\.\d+)$', line)
             # converts time from a string to datetime object
             lap_time = datetime.datetime.strptime(
-                matchObj.group(2).rstrip(), '%H:%M:%S.%f')
-            # adds key, value to a timing_log
-            timing_log[matchObj.group(1)] = lap_time
+                match_obj.group(2).rstrip(), '%H:%M:%S.%f')
+            timing_log[match_obj.group(1)] = lap_time
     return timing_log
 
 
-start_timings = retrieve_timings_from_log('start.log')
-end_timings = retrieve_timings_from_log('end.log')
-
-
-def sorted_individual_results(start_timings_, end_timings_, abbr_dict_, reverse_order=False):
+def sorted_individual_results(start_timings: Dict[str, datetime],
+                              end_timings: Dict[str, datetime])\
+        -> Dict[str, int]:
     """
+    creating dict with best lap results
     Receives start and end timings and returns an OrderedDict with
-    {abbreviations:timedeltas}
+    {abbreviations:timedelta in minutes}
     """
-    # creating dict with best lap results
-    lap_results = {key: end_timings_[key] - start_timings_.get(key, 0)
-                   for key in start_timings_.keys()}
-    sorted_results = dict(
-        sorted(lap_results.items(), key=lambda x: x[1], reverse=reverse_order))
+
+    keys = set(start_timings.keys()).intersection(set(end_timings.keys()))
+    lap_results = {
+        key: (end_timings[key] - start_timings[key]).seconds // 60
+        for key in keys
+    }
+    sorted_results = OrderedDict(
+        sorted(lap_results.items(), key=lambda x: x[1]))
     return sorted_results
 
 
-sorted_lap_results = sorted_individual_results(
-    start_timings, end_timings, abbr_dict)
-
-# prints result board to a console
-def print_result_board(sorted_lap_results_):
+def print_result_board(sorted_lap_results: Dict[str, int],
+                       abbreviations: Dict[str, tuple[str, str]]):
+    """
+    prints result board to a console
+    """
     counter = 1
-    for key, value in sorted_lap_results_.items():
-        racer_name = abbr_dict[key][0]
-        team_name = abbr_dict[key][1]
-        best_time = str(value)[2:-3]
+    for abbreviation, time in sorted_lap_results.items():
+        racer_name = abbreviations[abbreviation][0]
+        team_name = abbreviations[abbreviation][1]
         print(("{: <3} {: <18} | {: <30}  | {}".format(
-            str(counter)+'.', racer_name, team_name, best_time)))
+            str(counter) + '.', racer_name, team_name, str(time)))
+        )
         if counter == 15:
-            print(
-                '----------------------------------------------------------------------')
+            print('-'*70)
         counter += 1
 
 
-print_result_board(sorted_lap_results)
+def main():
+    abbreviations = create_racer_abbreviations_dict('abbreviations.txt')
+    start_timings = retrieve_timings_from_log('start.log')
+    end_timings = retrieve_timings_from_log('end.log')
+    sorted_lap_results = sorted_individual_results(start_timings, end_timings)
+    print_result_board(sorted_lap_results, abbreviations)
